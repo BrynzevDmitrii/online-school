fdf4bff98b452b165e5eac8ce1ce884dc940be9f
Dmitry Bryntsev <dmitry.bryntsev@legalbet.com>
2024-10-30 21:41:20 +0300

    add eslint & prettier

--- components/share/header.tsx
+++ components/share/header.tsx
@@ -1,37 +1,37 @@
 'use client'
-import Container from "@/components/share/Containers/container";
-import Image from "next/image";
-import { Button } from "../ui/button";
-import React from "react";
-import {useResize} from "@/hooks/useResize";
-import MobileHeader from "@/components/share/MobileHeader/mobileHeader";
+import Container from '@/components/share/Containers/container'
+import Image from 'next/image'
+import { Button } from '../ui/button'
+import React from 'react'
+import { useResize } from '@/hooks/useResize'
+import MobileHeader from '@/components/share/MobileHeader/mobileHeader'
 export default function Header() {
-    const menu = ['Курсы', 'Мастер-классы', 'Акции', 'Контакты', 'О нас']
-    const[active, setActive] = React.useState(0)
-    const{ isMobile } = useResize()
-    console.log(isMobile)
-    return (
-        isMobile?
-        <MobileHeader/> :
-        <Container>
-            <header className={'flex justify-between items-center'}>
-                <Image src={'/logo.png'} alt={'logo'} width={100} height={100} />
-                    <ul className={'flex  gap-6 text-black text-2xl'}>
-                        { menu.map((item, index) => {
-                            return (
-                                <li key ={item + index} className={'cursor-pointer hover:text-red-400'}>
-                                    <Button variant={active === index ? "default" : "secondary"}
-                                    onClick={() => setActive(index)}>{item}</Button>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                <div className={'flex items-center gap-4'}>
-                    <div>{'Регистрация / Войти'}</div>
-                    <Image src={'/icons8-user-48.png'} alt={'user-icon'} width={25} height={25}/>
-                </div>
-            </header>
-        </Container>
-
-    );
-};
+  const menu = ['Курсы', 'Мастер-классы', 'Акции', 'Контакты', 'О нас']
+  const [active, setActive] = React.useState(0)
+  const { isMobile } = useResize()
+  console.log(isMobile)
+  return isMobile ? (
+    <MobileHeader />
+  ) : (
+    <Container>
+      <header className={'flex justify-between items-center'}>
+        <Image src={'/logo.png'} alt={'logo'} width={100} height={100} />
+        <ul className={'flex  gap-6 text-black text-2xl'}>
+          {menu.map((item, index) => {
+            return (
+              <li key={item + index} className={'cursor-pointer hover:text-red-400'}>
+                <Button variant={active === index ? 'default' : 'secondary'} onClick={() => setActive(index)}>
+                  {item}
+                </Button>
+              </li>
+            )
+          })}
+        </ul>
+        <div className={'flex items-center gap-4'}>
+          <div>{'Регистрация / Войти'}</div>
+          <Image src={'/icons8-user-48.png'} alt={'user-icon'} width={25} height={25} />
+        </div>
+      </header>
+    </Container>
+  )
+}
